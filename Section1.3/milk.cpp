ad14f65cea94b6f9ddff31e515e1e7104e28638c
Jianfeng Hou <derekwangyi@163.com>
2017-08-08 20:07:58 +0800

    Update milk.cpp

--- Section1.3/milk.cpp
+++ Section1.3/milk.cpp
@@ -1,1 +1,48 @@
+/*
+ID: 
+PROB: milk 
+LANG: C++
+*/
 
+#include <fstream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    // Input
+    ifstream fin("milk.in");
+    int N, M;
+    fin >> N >> M;
+    map<int, int> farmersPriceAmount = map<int, int>();
+    int currentPrice, currentAmount;
+    for (int i = 0; i < M; i++) {
+        fin >> currentPrice >> currentAmount;
+        if (farmersPriceAmount.find(currentPrice) != farmersPriceAmount.end()) {
+            farmersPriceAmount[currentPrice] += currentAmount;
+        }
+        else {
+            farmersPriceAmount[currentPrice] = currentAmount;
+        }
+    }
+    fin.close();
+    
+    // Output
+    ofstream fout("milk.out");
+    int purchasedAmount = 0;
+    int cost = 0;
+    for (map<int, int>::iterator iter = farmersPriceAmount.begin(); iter != farmersPriceAmount.end(); iter++) {
+        if (iter->second > (N - purchasedAmount)) {
+            cost += iter->first * (N - purchasedAmount);
+            break;
+        }
+        else {
+            cost += iter->first * iter->second;
+            purchasedAmount += iter->second;
+        }
+    }
+    fout << cost << std::endl;
+    fout.close();
+    
+    return 0;
+}
