9c0715b0d5d70bb7d79cfc2b3ef88c698dc3a6eb
Jianfeng Hou <derekwangyi@163.com>
2017-08-13 00:24:47 +0800

    Update ariprog.cpp
    Solved. The previous solution uses std::set<int> to store the bisquare numbers, but the time efficiency is too slow for the time limit. Next time I will firstly use arrays other than C++ STL containers.

--- Section1.4/ariprog.cpp
+++ Section1.4/ariprog.cpp
@@ -5,8 +5,7 @@ LANG: C++
 */
 
 #include <fstream>
-#include <set>
-#include <algorithm> // For std::find
+#include <algorithm> // For std::fill
 
 using namespace std;
 
@@ -18,35 +17,25 @@ int main() {
     fin.close();
     
     // Generate the bisquare set
-    set<int> bisquares = set<int>();
-//    int count = 0; // Derek is debugging...
+    bool bisquaresSet[1000000];
+    fill(bisquaresSet, bisquaresSet + 1000000, false);
     for (int i = 0; i <= M; i++) {
         for (int j = 0; j <= M; j++) {
-            bisquares.insert(i*i + j*j);
-//            count++; // Derek is debugging...
+            bisquaresSet[i*i + j*j - 1] = true;
         }
     }
     
     // Output
     ofstream fout("ariprog.out");
-    //////////////////////////////////////////////
-    // Derek is debugging...
-//    for (set<int>::iterator iter = bisquares.begin(); iter != bisquares.end(); iter++) {
-//        fout << *iter << endl;
-//    }
-//    fout << "The size of the bisquare set: " << bisquares.size() << endl;
-//    fout << "count = " << count << endl; 
-    //////////////////////////////////////////////
     int largestBisquare = 2 * M * M;
     int b = 1;
     bool hasAnswer = false;
     do {
         int end = largestBisquare - b * (N - 1);
-//        fout << "    " << "end = " << end << endl; // Derek is debugging...
         for (int begin = 0; begin <= end; begin++) {
             bool isFeasible = true;
             for (int i = 0; i < N; i++) {
-                if (find(bisquares.begin(), bisquares.end(), begin + b * i) == bisquares.end()) {
+                if (!bisquaresSet[begin + b * i - 1]) {
                     isFeasible = false;
                     break;
                 }
